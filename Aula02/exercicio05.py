b47418f73efc13a985da308aa6085e48584f8640
Kauan494 <kauan.elias.ribeiro@gmail.com>
2025-02-25 10:46:27 -0300

    .

--- Aula02/exercicio05.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Digite um valor:"))
-
-if a < 0:
-    print("Negativo")
-else:
-    print("Positivo")
---
+++ Aula03/exercicio06.py
@@ -0,0 +1,16 @@
+a = int(input("Digite um numero de 1 a 7:"))
+
+if a == 1:
+    print("Domingo")
+elif a == 2: 
+    print("Segunda")
+elif a == 3:
+    print("Terça")
+elif a == 4:
+    print("Quarta")
+elif a == 5:
+    print("Quinta")
+elif a == 6:
+    print("Sexta")
+elif a == 7:
+    print("Sabádo")
