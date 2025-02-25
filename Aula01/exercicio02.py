dfdc006ec3ddfbbcdb9e3e54f4d4767594250f4c
Kauan494 <kauan.elias.ribeiro@gmail.com>
2025-02-25 10:39:12 -0300

    exercicios

---
+++ Aula01/exercicio02.py
@@ -0,0 +1,4 @@
+Kmrodado = int(input("Digite a quantidade de Km rodados:"))
+dia = int(input("Digite a quantidade de dias:"))
+Totalapagar = (60*dia + 0.15*Kmrodado)
+print("Total a pagar é: ", Totalapagar)
---
+++ Aula01/exercio01.py
@@ -0,0 +1,3 @@
+altura = float(input("Informe sua altura:"))
+pesoideal = float(72.7*altura)-58
+print("Seu peso ideal é:", pesoideal)
